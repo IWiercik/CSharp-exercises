dd27fa8e2ab40cbaab96bc45258b73dcae2ac4cb
unknown <wiercikdoc@gmail.com>
2022-11-02 10:28:49 +0100

    Added program for finding  second highest number

---
+++ 8.2.5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("How much numbers you want to write?");
+            string userInput = Console.ReadLine();
+            while (!(int.TryParse(userInput, out number)))
+            {
+                Console.WriteLine("incorret value \n Type amount of numbers you want to write!");
+                userInput = Console.ReadLine();
+            }
+            int amountOfUserNumbers = int.Parse(userInput);
+
+            int[] numbers = new int[amountOfUserNumbers];
+            for (int i = 0; i < amountOfUserNumbers; i++)
+            {
+                int counterOfNumbers = i + 1;
+                Console.WriteLine("Pass your " + counterOfNumbers + " number:");
+                string userInputNumber = Console.ReadLine();
+                while (!(int.TryParse(userInputNumber, out number)))
+                {
+                    Console.WriteLine("Inncorect value! \n Pass your number!");
+                    userInputNumber = Console.ReadLine();
+                }
+                numbers[i] = int.Parse(userInputNumber);
+            }
+            Console.WriteLine("Array view:");
+            showingArrayContent(numbers);
+            Console.WriteLine("Second highest number is:" + gettingSecondHighest(numbers));
+
+
+        }
+        static void showingArrayContent(int[] table)
+        {
+            string txt = "[";
+            for (int i = 0; i < table.Length; i++)
+            {
+                txt += table[i];
+                if (i < table.Length - 1)
+                {
+                    txt += ", ";
+                }
+            }
+            txt += "]";
+            Console.WriteLine(txt);
+        }
+        static int gettingSecondHighest(int[] table)
+        {
+            int highestNumber = table.Max();
+            int secondHighestNumber = table.Min();
+            //Getting secondHighest
+            foreach (int number in table)
+            {
+                if (number > secondHighestNumber && number < highestNumber)
+                {
+                    secondHighestNumber = number;
+                }
+            }
+            return secondHighestNumber;
+
+        }
+    }
+}
