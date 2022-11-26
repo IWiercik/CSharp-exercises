e9b183eaebeba3c05e228a94e3a7eedd18bd9111
unknown <wiercikdoc@gmail.com>
2022-11-26 16:59:25 +0100

    Added conversion dec->Bin

---
+++ Conversions/[X]_To_Bin/Dec_To_Bin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DecToBin(1));
+        }
+
+        static string DecToBin(int decimalValue)
+        {
+            string bin = "";
+            int dec = decimalValue;
+            while (dec > 0)
+            {
+                bin = bin.Insert(0, (dec % 2).ToString());
+                dec = dec / 2;
+            }
+            return bin;
+        }
+    }
+}
