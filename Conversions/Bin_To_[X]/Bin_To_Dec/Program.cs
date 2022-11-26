762c7e7ba4b411165d79b7faaea6f24b69f61d6e
unknown <wiercikdoc@gmail.com>
2022-11-26 15:52:37 +0100

    Added first three conversions Bin->X

---
+++ Conversions/Bin_To_[X]/Bin_To_Dec/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinToDec("11111111"));
+        }
+        static double BinToDec(string binary)
+        {
+            double decValue = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                int number = binary[i] - '0' ; // Convert string to number by - "0"
+                double power = binary.Length - 1 -i;
+                decValue += Math.Pow(2,power)*number;
+            }
+            return decValue;
+        }
+    }
+}
---
+++ Conversions/Bin_To_[X]/Bin_To_Hex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BinConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int decValue = BinToDec("1111111110");
+            string hexValue = decValue.ToString("X");
+            Console.WriteLine(hexValue);
+        }
+
+        static int BinToDec(string binary)
+        {
+            double decValue = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                int number = binary[i] - '0'; // Convert string to number by - "0"
+                double power = binary.Length - 1 - i;
+                decValue += Math.Pow(2, power) * number;
+            }
+            return (int) decValue;
+        }
+    }
+}
