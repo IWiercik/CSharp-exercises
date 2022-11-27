6edb64c7464fee4c06eb310ba2e0225331a70ab4
unknown <wiercikdoc@gmail.com>
2022-11-28 00:19:28 +0100

    Added [x] -> [x] conversions

---
+++ Conversions/[X]_To_[X]/Dec_To_Hex/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BinConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decToHex(188);
+        }
+        static void decToHex(int decValue)
+        {
+            int startingValue = decValue;
+            string result = "";
+            while (startingValue > 0)
+            {
+                switch (startingValue % 16)
+                {
+                    case 10:
+                        result = "A" + result;
+                        break;
+                    case 11:
+                        result = "B" + result;
+                        break;
+                    case 12:
+                        result = "C" + result;
+                        break;
+                    case 13:
+                        result = "D" + result;
+                        break;
+                    case 14:
+                        result = "E" + result;
+                        break;
+                    case 15:
+                        result = "F" + result;
+                        break;
+                    default:
+                        result = startingValue % 16 + result;
+                        break;
+                }
+                startingValue = startingValue / 16;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Conversions/[X]_To_[X]/Hex_To_Dec/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BinConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HexToDec("AA");
+        }
+        static void HexToDec(string hexValue)
+        {
+            string startingValue = hexValue;
+            int result = 0;
+            for (int i = 0; i < startingValue.Length; i++)
+            {
+                char actualChar = startingValue[i];
+                int charDecValue;
+                switch (actualChar)
+                {
+                    case 'A':
+                        charDecValue = 10;
+                        break;
+                    case 'B':
+                        charDecValue = 11;
+                        break;
+                    case 'C':
+                        charDecValue = 12;
+                        break;
+                    case 'D':
+                        charDecValue = 13;
+                        break;
+                    case 'E':
+                        charDecValue = 14;
+                        break;
+                    case 'F':
+                        charDecValue = 15;
+                        break;
+                    default:
+                        charDecValue = (int) actualChar;
+                        break;
+                }
+                result += (int)(Math.Pow(16, startingValue.Length - i - 1) * charDecValue);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
